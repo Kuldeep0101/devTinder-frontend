c9d6eaedba93850e766c78d8e1f8a40854ac6b3b
Kuldeep0101 <kuldeepsingh44682@gmail.com>
2025-08-26 18:27:23 +0530

    Added- NavBar, Body, Footer component with Routing to differnt routes using React-Router-DOM

---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+const Login = () => {
+    return (
+        <>
+            <h1 class="text-red-500">Welcome to login page</h1>
+        </>
+    )
+}
+
+export default Login
